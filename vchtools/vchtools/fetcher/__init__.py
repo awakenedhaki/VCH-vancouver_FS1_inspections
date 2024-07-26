14782d87a6a3877377f6ced59bed2382110d85de
Rodrigo Vallejos <42015094+awakenedhaki@users.noreply.github.com>
2024-07-26 13:51:47 -0700

    feat: Add fetcher module with threaded and synchronous request handlers

---
+++ vchtools/vchtools/fetcher/__init__.py
@@ -0,0 +1,10 @@
+from vchtools.fetcher.threaded import (
+    ThreadedRequestHandlerMixin,
+    ThreadedGETRequestHandler,
+    ThreadedPOSTRequestHandler,
+)
+from vchtools.fetcher.synchronous import (
+    APIHandler,
+    GETRequestHandler,
+    POSTRequestHandler,
+)
